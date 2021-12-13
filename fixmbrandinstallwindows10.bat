302070fb7ab674801b3f4d120f23fdb73a0e1f2d
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2021-12-13 14:56:48 +0200

    Add files via upload

---
+++ fixmbrandinstallwindows10.bat
@@ -0,0 +1,18 @@
+@echo off
+diskpart 
+sel vol 0 
+del vol 
+creat par prim size=250000
+format fs=ntfs QUICK COMPRESS
+assign letter V
+active
+exit  
+title FIX MBR
+bootsect /nt60 C: /mbr
+bcdboot D:\windows /s C:
+title Install Windows 10 Pro
+dism /export-image /SourceImageFile:D:\sources\install.esd /SourceIndex:4 /DestinationImageFile:C:\InstallWindows10\install.wim /Compress:max /CheckIntegrity
+dism /Apply-Image /ImageFile:C:\InstallWindows10\install.wim /Index:4 /ApplyDir:C:\
+title DONE!!!!
+pause
+exit
